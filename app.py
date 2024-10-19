5313560be62724e8f451cf5505364787605a22dc
Dien Vu Van <141806371+KillerBee244@users.noreply.github.com>
2024-10-20 06:36:07 +0700

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import numpy as np
 import pandas as pd
 from sklearn.metrics import r2_score, mean_squared_error, mean_absolute_error
 from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
 import joblib
 app = Flask(__name__)
 
